20dcad2599deb5aa36c368f900ba0f40664e10b6
Tsuser <tsuser@WISE.SVECW>
2017-07-02 03:15:06 -0500

    First commit

--- samapp/models.py
+++ samapp/models.py
@@ -22,5 +22,55 @@ class laundries(models.Model):
     sector_id = models.CharField(max_length = 10)
     phone_no = models.IntegerField(default=10)
     def __str__(self):
-            return self.name
-
+        return self.name
+class cities(models.Model):
+    city = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+class areas(models.Model):
+    area = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+class city_areamap(models.Model):
+    city_id = models.ForeignKey(cities,max_length=50)
+    area_id = models.ForeignKey(areas,max_length=50)
+    latitude = models.CharField(max_length=50)
+    longitude = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+class sectors(models.Model):
+    sectors = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+class dresses(models.Model):
+    dresses = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+class services(models.Model):
+    service = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+class dress_servicemap(models.Model):
+    dress_id = models.CharField(max_length=50)
+    service_id = models.CharField(max_length=50)
+    cost = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+class excess_cost(models.Model):
+    laundry_id = models.ForeignKey(laundries,max_length=50)
+    days = models.CharField(max_length=50)
+    cost = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+class orders(models.Model):
+    address = models.CharField(max_length=50)
+    phno = models.CharField(max_length=50)
+    date = models.CharField(max_length=50)
+    time = models.CharField(max_length=50)
+    laundry_id = models.ForeignKey(laundries,max_length=50)
+    dress_service_map_id = models.ForeignKey(dress_servicemap,max_length=50)
+    excess_cost_id = models.ForeignKey(excess_cost,max_length=50)
+    user_id = models.CharField(max_length=50)
+    rating = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
--- samapp/urls.py
+++ samapp/urls.py
@@ -1,3 +1,13 @@
 from django.conf.urls import url
+from django.contrib import admin
 from samapp import views
-urlpatterns = [url(r'^$', views.index, name = 'home'),]
+urlpatterns = [url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name = "home"),
+    url(r'^[A-Za-z]laundry_search$', views.laundry_search, name = "laund    ry_search"),
+    url(r'^[A-Za-z]shop_view$', views.shop_view, name = "shop_view"),
+    url(r'^[A-Za-z]count_clothes$', views.count_clothes, name = "count_clothes"),
+    url(r'^[A-Za-z]fast_delivery$', views.fast_delivery,name = "fast_delivery"),    url(r'^[A-Za-z]total_cost$', views.total_cost,name = "total_cost"),
+    url(r'^[A-Za-z]address$', views.address,name = "address"),
+    url(r'^[A-Za-z]success_message$', views.success_message, name = "suc    cess_message"),
+    url(r'^[A-Za-z]ratings$', views.ratings, name = "ratings"),
+]
